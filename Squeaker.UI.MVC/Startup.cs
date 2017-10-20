225688e0bdf032755d8fe41b9eb58ae106ed90a9
kriszek <kriszek@somewhere.com>
2017-10-20 19:27:48 +0200

    created basic projects

---
+++ Squeaker.UI.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Squeaker.UI.MVC.Startup))]
+namespace Squeaker.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
